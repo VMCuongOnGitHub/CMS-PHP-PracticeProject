3ec4bfc18fb4c9509df7ec9237304e0890b176ae
Cuong <vmc529440@gmail.com>
2018-09-30 21:29:19 +0700

    Create a dynamic post page (SEC12/89)

--- index.php
+++ index.php
@@ -11,25 +11,45 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+                <?php
+                  // SELECT the elements that you want to display from database
+                  $query = "SELECT * FROM post";
+                  // Make a connection to database and execute the querry
+                  $select_all_post_query = mysqli_query($connection, $query);
+                  // Use while loop to show the value
+                  while ($row = mysqli_fetch_assoc($select_all_post_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+
+                    echo "
+                    
+                    <h2>
+                        <a href='#'>{$post_title}</a>
+                    </h2>
+                    <p class='lead'>
+                        by <a href='index.php'>{$post_author}</a>
+                    </p>
+                    <p><span class='glyphicon glyphicon-time'></span> Posted on {$post_date} at 10:00 PM</p>
+                    <hr>
+                    <img class='img-responsive' src='{$post_image}' alt=''>
+                    <hr>
+                    <p>{$post_content}</p>
+                    <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
+                    <hr>
+
+                    ";
+                  }
+                ?>
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
+
 
             </div>
 
