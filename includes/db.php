d451287dcc7f8d70e2cd287fa1a41cc307cc8f68
Cuong <vmc529440@gmail.com>
2018-09-30 19:00:53 +0700

    Connect to the database (SEC12/84)

---
+++ includes/db.php
@@ -0,0 +1,19 @@
+<?php
+
+// Array of infor about host, user, password, database name. Just for easy to manage.
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_password'] = '';
+$db['db_database_name'] = 'cms-project';
+
+foreach ($db as $key => $value) {
+  define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE_NAME);
+
+if ($connection) {
+  echo('smt happle');
+}
+
+?>
